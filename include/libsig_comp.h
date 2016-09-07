29e2adf6a79b83bc70f877cfae3bf0a75f48a97f
Athanasios Oikonomou <athoik@gmail.com>
2016-09-07 19:11:32 +0300

    Remove define of PSignal into ePsignal

--- include/libsig_comp.h
+++ include/libsig_comp.h
@@ -6,6 +6,4 @@
 #define CONNECT(_signal, _slot) _signal.connect(sigc::mem_fun(*this, &_slot))
 #define CONNECT_EXTRA(_signal, _slot, extra_args...) _signal.connect(bind(sigc::mem_fun(*this, &_slot), extra_args))
 
-#define PSignal		ePsignal
-
 #endif // __LIBSIG_COMP_H
